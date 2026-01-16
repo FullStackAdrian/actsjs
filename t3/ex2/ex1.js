733a49122f7253bf63f7edd420a1ace84b1767ec
adri <adrian.g.job@gmail.com>
2026-01-16 19:20:32 +0100

    ex1

--- t3/ex2/ex1.js
+++ t3/ex2/ex1.js
@@ -1,26 +1,5 @@
-// estructura json
-//   [{
-//     userId: 10,
-//     id: 99,
-//     title: 'temporibus sit alias delectus eligendi possimus magni',
-//     body: 'quo deleniti praesentium dicta non quod\n' +
-//       'aut est molestias\n' +
-//       'molestias et officia quis nihil\n' +
-//       'itaque dolorem quia'
-//   },
-//   {
-//     userId: 10,
-//     id: 100,
-//     title: 'at nam consequatur ea labore ea harum',
-//     body: 'cupiditate quo est a modi nesciunt soluta\n' +
-//       'ipsa voluptas error itaque dicta in\n' +
-//       'autem qui minus magnam et distinctio eum\n' +
-//       'accusamus ratione error aut'
-//   }
-// ]
-
 const response = await fetch("https://jsonplaceholder.typicode.com/posts");
-const data = response.json();
+const data = await response.json();
 
 const container = document.getElementById("container");
 const ul = document.createElement("ul");
